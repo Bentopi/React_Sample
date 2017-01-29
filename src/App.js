08946a4e8ff0f5a41e31b7098365e220d849d6df
Brian Simmons <bsimmonsn@gmail.com>
2017-01-29 10:32:11 -0600

    modified app.js to use the css file instead of the scss, since it's now being precompiled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
 
 
 class CommentBox extends React.Component {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './App';
-import './index.scss';
 
 ReactDOM.render(
   <CommentBox />,
